57e7113852e58f068f79c9b5fd9db20b5856356d
ashraf <sadarsign@gmail.com>
2025-01-17 19:32:55 +0300

    added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className='bg-gray-900 text-white py-8 mt-12'>
-      <div className='container mx-auto px-6 sm:px-12 lg:px-16'>
-        <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8'>
+    <footer className='py-8 mt-12 text-white bg-gray-900'>
+      <div className='container px-6 mx-auto sm:px-12 lg:px-16'>
+        <div className='grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-4'>
           {/* Logo and Brand Name */}
           <div className='flex flex-col items-start'>
-            <div className='text-3xl font-bold text-blue-500 mb-4'>
+            <div className='mb-4 text-3xl font-bold text-blue-500'>
               TIIGSI PAGES
             </div>
-            <p className='text-gray-400 text-sm max-w-xs'>
+            <p className='max-w-xs text-sm text-gray-400'>
               A trusted platform for your web pages and development needs. Let’s
               build something amazing together.
             </p>
@@ -18,7 +18,7 @@ const Footer = () => {
 
           {/* Quick Links */}
           <div>
-            <h5 className='text-lg font-semibold text-gray-200 mb-4'>
+            <h5 className='mb-4 text-lg font-semibold text-gray-200'>
               Quick Links
             </h5>
             <ul className='space-y-2'>
@@ -55,7 +55,7 @@ const Footer = () => {
 
           {/* Contact Info */}
           <div>
-            <h5 className='text-lg font-semibold text-gray-200 mb-4'>
+            <h5 className='mb-4 text-lg font-semibold text-gray-200'>
               Contact Us
             </h5>
             <ul className='space-y-2'>
@@ -69,43 +69,43 @@ const Footer = () => {
               </li>
               <li className='text-gray-400'>Phone: +123 456 7890</li>
               <li className='text-gray-400'>
-                Address: 123 Main St, City, Country
+                Address: 123 Main St, Hargeisa, Somaliland
               </li>
             </ul>
           </div>
 
           {/* Social Media */}
           <div>
-            <h5 className='text-lg font-semibold text-gray-200 mb-4'>
+            <h5 className='mb-4 text-lg font-semibold text-gray-200'>
               Follow Us
             </h5>
             <div className='flex space-x-4'>
               <a
                 href='https://facebook.com'
                 className='text-gray-400 hover:text-blue-500'>
-                <i className='fab fa-facebook-f text-xl'></i>
+                <i className='text-xl fab fa-facebook-f'></i>
               </a>
               <a
                 href='https://twitter.com'
                 className='text-gray-400 hover:text-blue-500'>
-                <i className='fab fa-twitter text-xl'></i>
+                <i className='text-xl fab fa-twitter'></i>
               </a>
               <a
                 href='https://linkedin.com'
                 className='text-gray-400 hover:text-blue-500'>
-                <i className='fab fa-linkedin-in text-xl'></i>
+                <i className='text-xl fab fa-linkedin-in'></i>
               </a>
               <a
                 href='https://instagram.com'
                 className='text-gray-400 hover:text-blue-500'>
-                <i className='fab fa-instagram text-xl'></i>
+                <i className='text-xl fab fa-instagram'></i>
               </a>
             </div>
           </div>
         </div>
 
         {/* Bottom Copyright */}
-        <div className='text-center text-gray-400 mt-8'>
+        <div className='mt-8 text-center text-gray-400'>
           <p>&copy; 2025 TIIGSI PAGES. All rights reserved.</p>
         </div>
       </div>
